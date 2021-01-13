c8dca0905bcfb8f0291c7f614274c60c767a86fc
thecodeplanner <thecodeplanner@gmail.com>
2021-01-12 20:09:03 -0500

    commit tree fb2550e0ad232e5e1ec4ccadf57955a436c8bdf4

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,49 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([])
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const formData = {
+      firstName: firstName, 
+      lastName: lastName
+    }
+
+    const dataArray = [...submittedData, formData]
+    setSubmittedData(dataArray)
+     setFirstName('')
+     setLastName('')
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  })
 
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
+      
   );
 }
 
